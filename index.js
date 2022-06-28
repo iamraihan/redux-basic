a8f5340d89bed90f52cf3af1a19dc750eaf2bf65
iamraihan <trueraihan@gmail.com>
2022-06-28 16:07:05 +0600

    basic redux

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const redux = require("redux");
 
 //variable Declare
-const inCreaseNum = "INC_NUM"; //value type should be uppercase
+const inCreaseNum = "INC_NUM"; //value type should be capital letters
 const deCreaseNum = "DEC_NUM";
 const addNum = "ADD_NUM";
+
 //init state
 const initState = {
   num: 0,
 };
+
 //reducer
 const updateReducer = (state = initState, action) => {
   if (action.type === addNum)
@@ -20,16 +22,27 @@ const updateReducer = (state = initState, action) => {
       ...state,
       num: state.num + 1,
     };
+  if (action.type === deCreaseNum)
+    return {
+      ...state,
+      num: state.num - 1,
+    };
   return state;
 };
+
 //store
 const store = redux.createStore(updateReducer);
 
-console.log(store.getState());
+console.log(store.getState()); //getState function working manually
+
+store.subscribe(() => {
+  // subscribe function working automatically
+  console.log(store.getState());
+});
 
 //action dispatch
 store.dispatch({
-  type: addNum,
+  type: addNum, //we can set type value here also but good practice is declare is a variable which create top of the page.
   value: 55,
 });
 
@@ -37,4 +50,7 @@ store.dispatch({
   type: inCreaseNum,
 });
 
+store.dispatch({
+  type: deCreaseNum,
+});
 console.log(store.getState());
