fef881c0ffb63f32685642ea09b8c47063067ce5
iamraihan <trueraihan@gmail.com>
2022-06-28 15:46:24 +0600

    basic redux increase and ad num

--- index.js
+++ index.js
@@ -1,5 +1,40 @@
 const redux = require("redux");
 
-const store = redux.createStore();
+//variable Declare
+const inCreaseNum = "INC_NUM"; //value type should be uppercase
+const deCreaseNum = "DEC_NUM";
+const addNum = "ADD_NUM";
+//init state
+const initState = {
+  num: 0,
+};
+//reducer
+const updateReducer = (state = initState, action) => {
+  if (action.type === addNum)
+    return {
+      ...state,
+      num: state.num + action.value,
+    };
+  if (action.type === inCreaseNum)
+    return {
+      ...state,
+      num: state.num + 1,
+    };
+  return state;
+};
+//store
+const store = redux.createStore(updateReducer);
 
-console.log(store);
+console.log(store.getState());
+
+//action dispatch
+store.dispatch({
+  type: addNum,
+  value: 55,
+});
+
+store.dispatch({
+  type: inCreaseNum,
+});
+
+console.log(store.getState());
